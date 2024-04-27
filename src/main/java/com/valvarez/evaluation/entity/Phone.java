a73637955ef8199abf4edf7a8c1cb17903f8517a
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2024-04-27 13:48:44 -0400

    Agrego clases de entidad

---
+++ src/main/java/com/valvarez/evaluation/entity/Phone.java
@@ -0,0 +1,27 @@
+package com.valvarez.evaluation.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private String citycode;
+
+    @Column(nullable = false)
+    private String countrycode;
+}
---
+++ src/main/java/com/valvarez/evaluation/entity/User.java
@@ -0,0 +1,34 @@
+package com.valvarez.evaluation.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "user_id")
+    private List<Phone> phones;
+
+}
