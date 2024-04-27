2ee01e79dfcaf60b60d073003043fb9f5957e928
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2024-04-27 13:04:46 -0400

    rama principal con dependencias bases minimas necesarias

---
+++ src/main/java/com/valvarez/evaluation/ValvrezApplication.java
@@ -0,0 +1,13 @@
+package com.valvarez.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValvrezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValvrezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valvarez/evaluation/ValvrezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valvarez.evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValvrezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
